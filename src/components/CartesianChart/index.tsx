cc90eada446bc3f1361019280d9fd8a3df029814
Paulo <pauloccesar.131@gmail.com>
2024-05-14 22:07:08 -0300

    feat: create cartesianChartt componente

--- src/components/CartesianChart/index.tsx
+++ src/components/CartesianChart/index.tsx
@@ -1,47 +1,37 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import { CartesianChart, Line, useChartPressState } from "victory-native";
-import Animated, { SharedValue, useAnimatedProps } from 'react-native-reanimated'
 import { Circle, useFont } from '@shopify/react-native-skia';
-import { format } from  'date-fns'
+import { format } from 'date-fns';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Animated, { SharedValue, useAnimatedProps } from 'react-native-reanimated';
+import { CartesianChart, Line, useChartPressState } from 'victory-native';
 
-const DATA = [
-  { day: new Date("2024-04-09").getTime(), price: 600 },
-  { day: new Date("2024-04-10").getTime(), price: 500 },
-  { day: new Date("2024-04-11").getTime(), price: 630 },
-  { day: new Date("2024-04-12").getTime(), price: 420 },
-  { day: new Date("2024-04-13").getTime(), price: 900 },
-  { day: new Date("2024-04-14").getTime(), price: 940 },
-  { day: new Date("2024-04-15").getTime(), price: 820.90 },
-  { day: new Date("2024-04-18").getTime(), price: 1520 },
-]
+import { formatDolar } from '~/utils';
 
-Animated.addWhitelistedNativeProps({ text: true })
-const AnimatedTextInput = Animated.createAnimatedComponent(TextInput)
+Animated.addWhitelistedNativeProps({ text: true });
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
   return <Circle cx={x} cy={y} r={8} color="black" />;
 }
 
-export default function GraphicCartesian() {
-  const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 }})
-  // const font = useFont(require("./src/fonts/Roboto-Regular.ttf"))
-
+export default function GraphicCartesian({ data }) {
+  const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 } });
+  const font = useFont(require('../../fonts/Roboto-Regular.ttf'));
 
   const animatedText = useAnimatedProps(() => {
     return {
-      text: `R$ ${state.y.price.value.value.toFixed(2)}`,
-      defaultValue: ""
-    }
-  })
+      text: `$ ${state.y.price.value.value.toFixed(2)}`,
+      defaultValue: '',
+    };
+  });
 
   const animatedDateText = useAnimatedProps(() => {
     const date = new Date(state.x.value.value);
     return {
-      text: `R$ ${date.toLocaleDateString("pt-BR")}`,
-      defaultValue: ""
-    }
-  })
-
+      // text: `$ ${date.toLocaleDateString("pt-BR")}`,
+      text: `$ ${state.x.value.value}`,
+      defaultValue: '',
+    };
+  });
 
   return (
     <View style={styles.container}>
@@ -50,56 +40,49 @@ export default function GraphicCartesian() {
           <View>
             <AnimatedTextInput
               editable={false}
-              underlineColorAndroid={"transparent"}
-              style={{ fontSize: 30, fontWeight: 'bold', color: "#000" }}
+              underlineColorAndroid="transparent"
+              style={{ fontSize: 24, fontWeight: 'bold', color: '#000' }}
               animatedProps={animatedText}
-            ></AnimatedTextInput>
+            />
 
-            <AnimatedTextInput
+            {/* <AnimatedTextInput
               editable={false}
               underlineColorAndroid={"transparent"}
               animatedProps={animatedDateText}
-            ></AnimatedTextInput>
-
+            ></AnimatedTextInput> */}
           </View>
         )}
 
         {!isActive && (
           <View>
-             <Text
-               style={{ fontSize: 30, fontWeight: 'bold', color: "#000" }}
-             >
-              R${DATA[DATA.length - 1].price.toFixed(2)}
-             </Text>
-             <Text
-             >
-              Hoje
-             </Text>
+            <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#000' }}>
+              U${data[data.length - 1].price.toFixed(4)}
+            </Text>
+            <Text>Atual</Text>
           </View>
         )}
 
-        <CartesianChart 
-          data={DATA} xKey="day" yKeys={["price"]}
+        <CartesianChart
+          data={data}
+          xKey="seconds"
+          yKeys={['price']}
           chartPressState={state}
           axisOptions={{
-            tickCount: 5,
-            // font: font,
-            labelOffset: { x: 3, y: 2},
-            labelPosition: "inset",
-            formatYLabel: (value) => `${value}` ,
-            formatXLabel: (value) => format(new Date(), "MM/yy")
-          }}
-        >
-            {({ points }) => (
-              <>
-                <Line points={points.price} color="blue" strokeWidth={4} />
-                {isActive && (
-                  <ToolTip x={state.x.position} y={state.y.price.position} />
-                )}
-              </>
-            )}
+            tickCount: 12,
+            font,
+            labelOffset: { x: 2, y: 10 },
+            labelPosition: 'outset',
+            formatYLabel: (value) => `${formatDolar(value)}`,
+            formatXLabel: (value) => '',
+            // formatXLabel: (value) => `${value}`
+          }}>
+          {({ points }) => (
+            <>
+              <Line points={points.price} color="blue" strokeWidth={3} />
+              {isActive && <ToolTip x={state.x.position} y={state.y.price.position} />}
+            </>
+          )}
         </CartesianChart>
-
       </View>
     </View>
   );
@@ -107,12 +90,10 @@ export default function GraphicCartesian() {
 
 const styles = StyleSheet.create({
   container: {
-    // width: "100%",
-    // height: "100%",
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 20,
+    paddingHorizontal: 8,
   },
 });
